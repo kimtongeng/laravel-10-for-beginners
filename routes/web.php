ec6d3d84de8eff32bb8a929daca2d74815bfb508
kimtong <xnzzzxnzz@gmail.com>
2024-03-06 03:00:50 +0700

    day3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // $users = DB::select('select * from users');
+    // return view('welcome');
+    // $users = DB::insert("insert into users (name,email,password) values(?,?,?)",["tongtong","tongtong@gamil.com","1234"]);
+    // $users = DB::select("select * from users");
+    // $users = DB::update("update users set email = ? where id =?",["tongtong7@gamil.com","2"]);
+    // $users = DB::select("select * from users");
+    // $users = DB::delete("delete from users where id = 1");
+    // $users = DB::select("select * from users");
+
+    // $users = DB::table('users')->get();
+    // $users = DB::table('users')->where("id",2)->get();
+    // $users = DB::table('users')->insert([
+    //     'name'=>"tonglove",
+    //     'email'=>"tonglove1@gamil.com",
+    //     'password'=>"loveyou"
+    // ]);
+    // $users = DB::table("users")->where("id",3)->update(["name"=>"love meng"]);
+    // $users = DB::table('users')->where("id",2)->delete();
+
+    // $users = DB::table("users")->first();
+    // $users = DB::table("users")->where("id",3)->first();
+    // $users = DB::table("users")->find(3);
+
+
+    dd($users);
 });
 
 Route::get('/dashboard', function () {
